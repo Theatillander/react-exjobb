7122e9b35673720182cd96df4e9f0acf29b1378f
Theatillander <44837263+Theatillander@users.noreply.github.com>
2024-03-26 17:31:02 +0100

    Added icons and text.

--- frontend/src/components/InfoCard.jsx
+++ frontend/src/components/InfoCard.jsx
@@ -1,15 +1,57 @@
+import { useState } from 'react';
 import '../style/InfoCard.css';
+import QuestionAnswerIcon from '@mui/icons-material/QuestionAnswer';
+import LocalShippingIcon from '@mui/icons-material/LocalShipping';
+import RedeemIcon from '@mui/icons-material/Redeem';
+import PaymentIcon from '@mui/icons-material/Payment';
 
-function Navbar() {
+function InfoCard() {        
+    const [cards] = useState([
+        {
+            title: 'Free Shipping', 
+            text: 'some example text',
+            icon: <LocalShippingIcon/>
+        },
+        {
+            title: 'Free gift', 
+            text: 'some example text',
+            icon: <RedeemIcon/>
+        },
+        {
+            title: 'Online Support', 
+            text: 'some example text',
+            icon: <QuestionAnswerIcon/>
+        },
+        {
+            title: 'Flexible Payment', 
+            text: 'some example text',
+            icon: <PaymentIcon/>
+        }
+    ])
     return (
-
-      <div className="nav-content">
-        <h1>Hiii</h1>
+        <div>
+            <section>
+                <div className='container_infocard'>
+                        <div className='cards_infocard'>
+                            {
+                            cards.map((infocard, i) =>(
+                                <div key={i} className='infocard'>
+                                    {infocard.icon}
+                                    <h3>
+                                        {infocard.title}
+                                    </h3>
+                                    <p>
+                                        {infocard.text}
+                                    </p>
+                                </div> 
+                                ))
+                            }
+                        </div>        
+                </div>
+            </section>
         </div>
-        
-  
     );
   }
   
-  export default Navbar;
+  export default InfoCard;
   
