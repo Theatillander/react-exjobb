16a96a3cf0e9bf5cb098317ccc54d3ad7cadd4f7
Thea <thea.tillander@gmail.com>
2024-05-14 19:42:02 +0200

    Set iteration limit + downloadable json

--- frontend/src/components/ProductPageProducts.jsx
+++ frontend/src/components/ProductPageProducts.jsx
@@ -1,30 +1,70 @@
 import Card from './Card';
 import '../style/products.css';
 import React, { useState, useEffect } from 'react';
-var jsonData = ('frontend/public/cards.json');
+
+/*
+  OBS! 
+  För att skapa en ny mätserie måste man först gå in i localstorage -> "clear all", sedan kan scriptet starta
+  När koden är klar laddar den ner "hackerman.json" som innehåller alla mätvärden.
+*/
 
 function ProductPageProducts() {
 
-  var [data, setData] = useState([]);
+  const MAX_COUNT = 10; // ändra detta värdet för att få ut X antal mätningar
+
+  const [data, setData] = useState([]);
+
   const startTime = performance.now();
 
+  if (!localStorage.getItem('measurementCount')) {
+    localStorage.setItem('measurementCount', 0);
+  }
+
+  if (!localStorage.getItem('data')) {
+    localStorage.setItem('data', JSON.stringify([]));
+  }
+  
+  const count = parseInt(localStorage.getItem('measurementCount'));
+
   useEffect(() => {
-    setInterval(() => {
+
+    if (count < MAX_COUNT){
       fetch('/cards.json')
         .then(response => response.json())
         .then(jsonData => setData(jsonData))
         .catch(error => console.error('Error fetching data:', error));
-        setData([]);
-    }, 1000);
-  }, []);
 
-  useEffect(() => {
-    if (data.length == 1000){  /* i < 1000 */
-      const endTime = performance.now();
-      const totalTime = endTime - startTime;
-      console.log(`${totalTime}`);
+      if (data.length === 1000){ // Mystery
+        const endTime = performance.now();
+        const totalTime = endTime - startTime;
+        console.log(`${count}: ${totalTime}`);
+
+        let prevArr = JSON.parse(localStorage.getItem('data'));
+        prevArr.push(totalTime);
+        localStorage.setItem('data', JSON.stringify(prevArr));
+        console.log(prevArr);
+    
+        localStorage.setItem('measurementCount', count+1);
+      }
+  
+      setTimeout(() => {
+        setData([]);
+      }, 3000);
     }
-  }, [data]);
+
+    else if (count === MAX_COUNT){
+      const blob = new Blob([localStorage.getItem('data')], { type: 'application/json' });
+      const url = URL.createObjectURL(blob);
+      const link = document.createElement('a');
+      link.href = url;
+      link.setAttribute('download', 'hackerman');
+      document.body.appendChild(link);
+      link.click();
+      document.body.removeChild(link);
+      URL.revokeObjectURL(url);
+    }
+
+  }, [data])
 
   return (
     <div className='gallery'>
